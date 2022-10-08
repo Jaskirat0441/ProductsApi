ca94db50b5afd34482123f2c8c19ca74254cd766
Jaskirat0441 <singhjaskirat862@gmail.com>
2022-10-08 13:12:51 +0530

    my products api

---
+++ index.js
@@ -0,0 +1,21 @@
+const  express = require("express");
+const cors = require("cors");
+const app = express();
+
+const apiData = require("./data.json")
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+
+app.get("/" ,(req ,res)=>{
+    res.send("i m live");
+});
+
+// main api data
+app.get("/products" ,(req ,res)=>{
+    res.send(apiData);
+});
+
+app.listen(port,()=>{
+    console.log("im live again ");
+});
